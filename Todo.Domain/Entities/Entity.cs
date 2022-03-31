24d53e86fbd4c07010a656eaf7b13c23aed44210
Murilo Oliveira - Alumisoft <murilo.oliveira@alumisoft.com.br>
2022-03-31 11:42:18 -0300

    Criando os modelos base e inicio dos commands

---
+++ Todo.Domain/Entities/Entity.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Todo.Domain.Entities
+{
+    public abstract class Entity : IEquatable<Entity>
+    {
+        public Guid Id { get; private set; }
+
+        public Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public bool Equals(Entity other) => Id == other.Id;
+    }
+}
